80c08276aea3035d593f7baf0d197354439fa1d7
GoobyGooberman <gabby.cervantes@gmail.com>
2018-12-04 20:51:24 +0800

    Finished restock point

--- viewInventory.php
+++ viewInventory.php
@@ -29,50 +29,73 @@
                                 <th>Quantity</th>
                                 <th>Type</th>
                                 <th>Price</th>
-                                <th>Restock Point</th>
                                 <th class="text-center">Action</th>
                             </tr>
                         </thead>
                         <tbody>
 
                         <?php
-                            $result = mysqli_query($conn,'SELECT product.name AS productname, 
+
+                            $dateNow = date("Y-m-d");
+                            $monthAgo = date("Y-m-d", strtotime("-1 month"));
+//                            print_p($dateNow);
+//                            print_p($monthAgo);
+
+                            $allInventory = mysqli_query($conn, "SELECT product.name AS productname, 
                                                                 product.quantity AS quantity, 
                                                                 productType.name AS producttypename, 
                                                                 product.productPrice, 
-                                                                product.productID AS ID, 
-                                                                ROUND(AVG(productsales.quantity)) AS average
-                                                                FROM product 
-                                                                JOIN productType ON product.productTypeID=productType.productTypeID 
-                                                                JOIN productsales ON product.productID=productsales.productID 
-                                                                
-                                                                GROUP BY product.name ');
+                                                                product.productID AS ID
+                                                                FROM product
+                                                                JOIN productType ON product.productTypeID=productType.productTypeID
+                                                                ");
 
+                            $result = mysqli_query($conn,"SELECT product.name AS productname, 
+                                                                product.quantity AS quantity, 
+                                                                productType.name AS producttypename, 
+                                                                product.productPrice, 
+                                                                product.productID AS ID,
+                                                                ROUND(AVG(productsales.quantity)) AS restock
+                                                                FROM productsales
+                                                                JOIN product on productsales.productID=product.productID
+                                                                JOIN productType ON product.productTypeID=productType.productTypeID
+                                                                JOIN sales ON  productsales.salesID=sales.salesID
+                                                                WHERE sales.saleDate BETWEEN '$monthAgo' and '$dateNow'
+                                                                GROUP BY product.name
+                                                                ");
 
-//                            $result2 = mysqli_query($conn, "SELECT product.name, ROUND(AVG(productsales.quantity))
-//                                                                  FROM productsales
-//                                                                  JOIN product on productsales.productID=product.productID
-//                                                                  GROUP BY product.name ");
-//
-//
-//                            while ($row = mysqli_fetch_array($result2)){
-//                                $
-//                            }
+//                            $result2 = mysqli_query($conn, "SELECT product.name,
+//                                                                ROUND(AVG(productsales.quantity)) AS restock
+//                                                                FROM productsales
+//                                                                JOIN product on productsales.productID=product.productID
+//                                                                JOIN sales ON  productsales.salesID=sales.salesID
+//                                                                WHERE sales.saleDate BETWEEN '$monthAgo' and '$dateNow'
+//                                                                GROUP BY product.name");
 
+                            while($row = mysqli_fetch_array($allInventory)){
 
-                            while($row = mysqli_fetch_array($result)){
-                              $id = $row['ID'];
-                              $prodName = $row['productname'];
-                              $prodType = $row['producttypename'];
-                              $quantity = $row['quantity'];
-                              $price = $row['productPrice'];
-                              $reorderPoint = $row['average'];
+                                $id = $row['ID'];
+                                $prodName = $row['productname'];
+                                $prodType = $row['producttypename'];
+                                $quantity = $row['quantity'];
+                                $price = $row['productPrice'];
 
-                              if ($reorderPoint>$quantity){
-                                  echo '<div class="alert alert-warning"><strong>Warning!</strong> Product ';
-                                  echo $prodName;
-                                  echo ' has reached optimal restocking point. Restocking recommended';
-                                  echo '</div>';
+                              while ($row2 = mysqli_fetch_array($result)){
+                                  $id = $row2['ID'];
+                                  $prodName = $row2['productname'];
+                                  $prodType = $row2['producttypename'];
+                                  $quantity = $row2['quantity'];
+                                  $price = $row2['productPrice'];
+                                  $reorderPoint = $row2['restock'];
+
+                                  if ($reorderPoint>$quantity){
+                                      echo '<div class="alert alert-warning"><strong>Warning!</strong> Product ';
+                                      echo $prodName;
+                                      echo ' has reached optimal restocking point. Restocking recommended.';
+                                      echo $reorderPoint;
+                                      echo '</div>';
+                                  }
+
                               }
 
                                   echo '<tr>';
@@ -88,9 +111,7 @@
                                     echo '<td>';
                                       echo $price;
                                     echo'</td>';
-                                    echo '<td>';
-                                        echo $reorderPoint;
-                                    echo'</td>';
+
                                     echo '<td class="text-center">';
                                       echo '<a href="makeJobOrder.php?ids='.$id.'&name='.$prodName.'"><button type="button" class="btn btn-primary btn-sm">Restock</button></a> ';
                                     echo '</td>';
