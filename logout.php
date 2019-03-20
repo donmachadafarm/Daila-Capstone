3ee9ab6212ed90748526c0ba63a027e16e12af96
Don Mac <don_macadat@dlsu.edu.ph>
2019-03-20 13:12:33 +0800

    logoutfix

--- logout.php
+++ logout.php
@@ -1,5 +1,11 @@
 <?php
 include 'includes/sections/header.php';
 session_destroy();
-header('location:login.php');
-exit();
+// header('location:login.php');
+
+?>
+
+<script type="text/javascript">
+window.location = 'login.php';
+</script>
+// exit();
