66bce8885cc0ac342990fd2083b0d65a2099a413
GoobyGooberman <gabby.cervantes@gmail.com>
2019-05-19 09:14:37 +0800

    required search on demand report

--- demandReport.php
+++ demandReport.php
@@ -23,7 +23,7 @@ if (!isset($_SESSION['userType'])){
     <div class="row">
         <div class="col-lg-12" align="center">
             <h1 class="text-center" align="center"><br>
-                Product Sales Chart
+                Top Products
             </h1>
             <h6 class="text-center">
                 Enter a Date Range
@@ -119,79 +119,79 @@ if (isset($_POST['search'])){
 
 else{
 
-   $endDate = date("Y-m-d");
-   $startDate = date("Y-m-d",strtotime("-7 days"));
+//    $endDate = date("Y-m-d");
+//    $startDate = date("Y-m-d",strtotime("-7 days"));
 
-  echo '<h2 class="text-center">Transactions between ';
-  echo date("F d Y",strtotime("-7 days"));;
-  echo ' and ';
-  echo date("F d Y");
-  echo '</h2><br>';
+//   echo '<h2 class="text-center">Transactions between ';
+//   echo date("F d Y",strtotime("-7 days"));;
+//   echo ' and ';
+//   echo date("F d Y");
+//   echo '</h2><br>';
 
-  echo        '<table class="columns" align="center">';
-  echo            '<tr>';
-  echo                '<td><div id="piechart" style="width: 450px; height: 350px;border: 1px solid #ccc"></div></td>';
-  echo                '<td><div id="piechart2" style="width: 450px; height: 350px;border: 1px solid #ccc"></div></td>';
-  echo            '</tr>';
-  echo        '</table>';
-  echo        '<table class="columns" align="center">';
+//   echo        '<table class="columns" align="center">';
+//   echo            '<tr>';
+//   echo                '<td><div id="piechart" style="width: 450px; height: 350px;border: 1px solid #ccc"></div></td>';
+//   echo                '<td><div id="piechart2" style="width: 450px; height: 350px;border: 1px solid #ccc"></div></td>';
+//   echo            '</tr>';
+//   echo        '</table>';
+//   echo        '<table class="columns" align="center">';
   
-  $connect = mysqli_connect("localhost", "root", "", "capstone_daila");
-  $query = "SELECT product.name as name,SUM(productsales.quantity) as 'products sold'
-                  FROM productsales
-                  JOIN product on productsales.productID=product.productID
-                  JOIN sales on productsales.salesID=sales.salesID
-                  WHERE sales.saleDate BETWEEN '$startDate' AND '$endDate'
-                  GROUP BY product.name
-                  ORDER BY `products sold`  DESC
-                  LIMIT 5";
-  $result = mysqli_query($connect, $query);
-  $query2 = "SELECT product.name as name, SUM(productsales.subTotal) as 'total sales'
-                  FROM productsales
-                  JOIN product on productsales.productID=product.productID
-                  JOIN sales on productsales.salesID=sales.salesID
-                  WHERE sales.saleDate BETWEEN '$startDate' AND '$endDate'
-                  GROUP BY product.name
-                  ORDER BY `total sales`  DESC
-                  LIMIT 5";
-  $result2 = mysqli_query($connect, $query2);
+//   $connect = mysqli_connect("localhost", "root", "", "capstone_daila");
+//   $query = "SELECT product.name as name,SUM(productsales.quantity) as 'products sold'
+//                   FROM productsales
+//                   JOIN product on productsales.productID=product.productID
+//                   JOIN sales on productsales.salesID=sales.salesID
+//                   WHERE sales.saleDate BETWEEN '$startDate' AND '$endDate'
+//                   GROUP BY product.name
+//                   ORDER BY `products sold`  DESC
+//                   LIMIT 5";
+//   $result = mysqli_query($connect, $query);
+//   $query2 = "SELECT product.name as name, SUM(productsales.subTotal) as 'total sales'
+//                   FROM productsales
+//                   JOIN product on productsales.productID=product.productID
+//                   JOIN sales on productsales.salesID=sales.salesID
+//                   WHERE sales.saleDate BETWEEN '$startDate' AND '$endDate'
+//                   GROUP BY product.name
+//                   ORDER BY `total sales`  DESC
+//                   LIMIT 5";
+//   $result2 = mysqli_query($connect, $query2);
 
-  $query3 = "SELECT y.name as 'name', y.quantitySold 'Quantity Sold', y.saleDate FROM
-                (SELECT x.name, x.quantitySold, x.saleDate FROM
-                    (SELECT product.name as 'name', COALESCE (productsales.quantity, 0) as ' quantitySold' , COALESCE (sales.saleDate, 'NO SALE') as 'saleDate' 
-                        FROM product 
-                        LEFT JOIN productsales ON product.productID = productsales.productID 
-                        LEFT JOIN sales ON productsales.salesID = sales.salesID 
+//   $query3 = "SELECT y.name as 'name', y.quantitySold 'Quantity Sold', y.saleDate FROM
+//                 (SELECT x.name, x.quantitySold, x.saleDate FROM
+//                     (SELECT product.name as 'name', COALESCE (productsales.quantity, 0) as ' quantitySold' , COALESCE (sales.saleDate, 'NO SALE') as 'saleDate' 
+//                         FROM product 
+//                         LEFT JOIN productsales ON product.productID = productsales.productID 
+//                         LEFT JOIN sales ON productsales.salesID = sales.salesID 
                         
-                        UNION ALL 
+//                         UNION ALL 
                         
-                        SELECT product.name as 'name', COALESCE (productsales.quantity, 0) as ' quantitySold' , COALESCE (sales.saleDate, 'NO SALE') as 'saleDate'
-                        FROM productsales 
-                        LEFT JOIN product ON productsales.productID = product.productID 
-                        LEFT JOIN sales ON productsales.salesID = sales.salesID 
-                        WHERE product.name IS NULL
+//                         SELECT product.name as 'name', COALESCE (productsales.quantity, 0) as ' quantitySold' , COALESCE (sales.saleDate, 'NO SALE') as 'saleDate'
+//                         FROM productsales 
+//                         LEFT JOIN product ON productsales.productID = product.productID 
+//                         LEFT JOIN sales ON productsales.salesID = sales.salesID 
+//                         WHERE product.name IS NULL
                         
-                        UNION ALL 
+//                         UNION ALL 
                         
-                        SELECT product.name as 'name', COALESCE (productsales.quantity, 0) as ' quantitySold' , COALESCE (sales.saleDate, 'NO SALE') as 'saleDate'
-                        FROM sales 
-                        LEFT JOIN productsales ON sales.salesID = productsales.salesID 
-                        LEFT JOIN product ON productsales.productID = product.productID
-                    WHERE product.name IS NULL AND productsales.quantity IS NULL) x
-                    WHERE x.name IS NOT NULL AND x.saleDate BETWEEN '$startDate' AND '$endDate' OR x.quantitySold = 0) y
-                WHERE y.name IS NOT NULL AND y.quantitySold = 0";
-    $result3 = mysqli_query($connect, $query3);
+//                         SELECT product.name as 'name', COALESCE (productsales.quantity, 0) as ' quantitySold' , COALESCE (sales.saleDate, 'NO SALE') as 'saleDate'
+//                         FROM sales 
+//                         LEFT JOIN productsales ON sales.salesID = productsales.salesID 
+//                         LEFT JOIN product ON productsales.productID = product.productID
+//                     WHERE product.name IS NULL AND productsales.quantity IS NULL) x
+//                     WHERE x.name IS NOT NULL AND x.saleDate BETWEEN '$startDate' AND '$endDate' OR x.quantitySold = 0) y
+//                 WHERE y.name IS NOT NULL AND y.quantitySold = 0";
+//     $result3 = mysqli_query($connect, $query3);
 
-    echo            '<tr>';
-    echo            '<td>Products Not Sold: </td>';
-                    while($row = mysqli_fetch_array($result3x)){
-                        $productName = $row['name'];
-    echo                '<td>';
-    echo                $productName;
-    echo                '</td>';
-                    }
-    echo            '</tr>';
-    echo        '</table>';
+//     echo            '<tr>';
+//     echo            '<td>Products Not Sold: </td>';
+//                     while($row = mysqli_fetch_array($result3x)){
+//                         $productName = $row['name'];
+//     echo                '<td>';
+//     echo                $productName;
+//     echo                '</td>';
+//                     }
+//     echo            '</tr>';
+//     echo        '</table>';
 }
 ?>
 
